9183eb41bb66a3a67ff0950103d243f153cb56a1
veryoriginal7 <tuandatele2005@gmail.com>
2022-12-08 17:05:49 -0800

    submitting

---
+++ src/main/java/frc/robot/subsystems/elevator.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import  com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class elevator extends SubsystemBase {
+  public elevator() {
+    // Use inches as unit for encoder distances
+   ;
+public initialize(){
+  
+}
+  }
+}
